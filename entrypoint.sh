e22b1b5cfa73fb6db76936cbcce079539a3c021b
inaumovv <165304244+inaumovv@users.noreply.github.com>
2024-07-21 07:46:43 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 if [ "$DATABASE" = "postgres" ]
 then
-    # если база еще не запущена
+
     echo "Рано..."
 
-    # Проверяем доступность хоста и порта
+
     while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
       sleep 0.1
     done
@@ -13,7 +13,7 @@ then
     echo "Пора!"
 fi
 
-# Выполняем миграции
+
 python manage.py makemigrations
 python manage.py migrate
 
